3f2214262c995ddde81e15fabb34bbd7e2b2520d
Patrick Dobson <pat@downpatproductions.com>
2022-05-16 07:55:19 -0400

    Added untested app.js to check Dockerfile with git clone command

---
+++ app.js
@@ -0,0 +1,14 @@
+const http = require('node:http')
+
+const hostname = '0.0.0.0'
+const port = 8080
+
+const server = http.createServer((request, response) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('Here I am. Rock you like a hurricane!')
+})
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+})
